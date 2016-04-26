22e00284766ce5a1be97e4bfb218f9a7c07edba7
edasaur <edx.wang@gmail.com>
2016-04-26 12:16:10 -0700

    Authentication works completely now...I think?

--- mtgrecorder/models.py
+++ mtgrecorder/models.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 
 class Player(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    fun_fact = models.CharField(max_length = 200)
+    fun_fact = models.CharField(max_length = 200, default='', blank=True)
+
+def create_profile(sender, **kwargs):
+    user = kwargs["instance"]
+    if kwargs["created"]:
+        player = Player(user=user)
+        player.save()
+
+post_save.connect(create_profile, sender=User)
--- mtgrecorder/views.py
+++ mtgrecorder/views.py
@@ -1,29 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from forms import CustomUserCreationForm, CustomPlayerCreationForm
+from forms import CustomUserCreationForm
 from django.core.context_processors import csrf
 from django.contrib.auth.decorators import login_required
 from models import Player
+from django.contrib.auth.models import User
+from django.forms.models import inlineformset_factory
 #def loggedin(request):
 #    return render(request, 'registration/loggedin.html', context={'username': request.user.username})
 
 def register(request):
+    #user = User(username="", password="")
+    form_user = CustomUserCreationForm()#instance=user)
+    ProfileInlineFormset = inlineformset_factory(User, Player, fields=('fun_fact',))
+    formset = ProfileInlineFormset()#instance=user)
     if request.method == 'POST':
-        form_user = CustomUserCreationForm(request.POST)
-        form_player = CustomPlayerCreationForm(request.POST)
-        if (form_user.is_valid() and form_player.is_valid()):
-            user = form_user.save()
-            player = form_player.save(commit=False)
-            player.user = user
-            player.save()
-            return HttpResponseRedirect('/register/complete')
-    else:
-        form_user = CustomUserCreationForm()
-        form_player = CustomPlayerCreationForm()
+        form_user = CustomUserCreationForm(request.POST)#, instance=user)
+        formset = ProfileInlineFormset(request.POST)#, instance=user)
+        if form_user.is_valid():
+            created_user = form_user.save(commit=False)
+            formset = ProfileInlineFormset(request.POST, instance=created_user)
+            if formset.is_valid():
+                created_user.save()
+                #formset.save()
+                return HttpResponseRedirect('/register/complete')
     token = {}
     token.update(csrf(request))
-    token['form_user'] = form_user
-    token['form_player'] = form_player
+    token['noodle_form'] = form_user
+    token['formset'] = formset
     return render(request, 'registration/registration_form.html', context=token)
 
 def registration_complete(request):
